7907a2f77ae10aee98fd1d9404ce67f620b4b865
Shubham Dwivedi <51696142+shubh-dwivedi@users.noreply.github.com>
2022-09-05 19:05:18 +0530

    Assignment complete

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,44 +4,41 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
-// import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
+// import CircularProgress from '@mui/material/CircularProgress';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const HomePage = () => {
   const [lyrics, setLyrics] = useState([]);
   const [lyricsSearch, setlyricsSearch] = useState({songName: '', artistName: ''});
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
   const fetchLyrics = async ()=> {
     try {
       let track = lyricsSearch.songName;
       let artist =  lyricsSearch.artistName;
-      // artist = artist.split(' ').join('%20');
       if(track && artist) {
         let url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${track}&q_artist=${artist}&apikey=39de21a625434384c0200df4e4daf7e8`
         const url2 = `https://musicapitesting.shubhdwivedi1.repl.co/translate?text=${url}`
-
+        setError("");
+        setLyrics([]);
+        setLoading(true);
         const data = await fetch(url2);
         const dataParsed = await data.json();
-        console.log(dataParsed);
+        // console.log(dataParsed);
+        setLoading(false);
         if(dataParsed.message.header.status_code === 404) {
-          console.log('Error: ' + dataParsed.message.header.status_code + ' Song not found');
+          setError("Lyrics not found! Please check the artist name and song name & try again.")
         } else {
           let dataLyrics = dataParsed.message.body.lyrics.lyrics_body;
           dataLyrics = dataLyrics.split('\n');
           setLyrics(dataLyrics);
-          // console.log(dataLyrics);
-          // setLyrics(dataParsed.message.body.lyrics.lyrics_body);
-          // console.log(dataParsed.message.header.status_code);
-          // console.log(dataParsed.message.body.lyrics.lyrics_body);
-          setError("");
         }
       } else {
-        setError('Please provide song and artist name to get lyrics!');
+        setError('Please enter song name and artist name to get lyrics!');
       }
-      
-        
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      setError('An Error has occured! Please try again later.');
     }
   }
 
@@ -52,7 +49,7 @@ const HomePage = () => {
   return (
     <Box component="main"  sx={{ p: 0, }}>
         <Toolbar />
-        <Typography component="h1" variant="h4" sx={{ textAlign: 'center', }}>
+        <Typography component="h1" variant="h4" sx={{ textAlign: 'center', fontFamily: 'monospace'}}>
           Welcome to Lyrics Finder App
         </Typography>
         <Box sx={{ textAlign: 'center', }} className='input-container'>
@@ -60,10 +57,14 @@ const HomePage = () => {
           <TextField id="outlined-basic" label="Artist Name" variant="outlined" className='input-field' name='artistName' onChange={handleChange}/>
           <Button onClick={()=>{fetchLyrics()}}>Find Song Lyrics</Button>
         </Box>
-        {error ? <Typography sx={{ textAlign: 'center', color: 'red'}}>{error}</Typography>
+        {loading && <Box sx={{ width: '30%', margin: '2rem auto' }}>
+                      <LinearProgress />
+                    </Box>
+        }
+        {error ? <Typography sx={{ textAlign: 'center', fontSize: '1.5rem', color: 'red'}}>{error}</Typography>
         : lyrics.length > 0 && <Box className='output-container'>
           <Typography component="h1" variant="h4" sx={{ textAlign: 'center', }}>
-           ----- Lyrics of {lyricsSearch.songName} by {lyricsSearch.artistName} -----
+           ----- Lyrics of the Song -----
           </Typography>
           <Typography component={"div"} sx={{textAlign: 'center', fontSize: '1.25rem', padding: '1rem'}}>
             {lyrics.map((lyric) => {
@@ -72,7 +73,6 @@ const HomePage = () => {
           </Typography>
         </Box>
         }
-        
     </Box>
   )
 }
